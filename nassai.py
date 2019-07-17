91988319bbe94eb13fc13d01cbe5a2bfa04d2e76
Olamyy <olamyy53@gmail.com>
2019-07-17 19:51:43 +0100

    Oya na

--- nassai.py
+++ nassai.py
@@ -1,4 +1,5 @@
 import csv
+import os
 from datetime import datetime
 
 import click
@@ -9,6 +10,21 @@ from code.sklearn_classifiers import BernNB, SVM, LinearSVM, MLP
 from code.train import train
 from code.utils import get_path, load_model
 from code.build import BuildEmbeddingModel
+
+from keras import backend as K
+import tensorflow as tf
+
+NUM_PARALLEL_EXEC_UNITS = 4
+config = tf.ConfigProto(intra_op_parallelism_threads=NUM_PARALLEL_EXEC_UNITS, inter_op_parallelism_threads=2,
+                        allow_soft_placement=True, device_count={'CPU': NUM_PARALLEL_EXEC_UNITS})
+session = tf.Session(config=config)
+K.set_session(session)
+os.environ["OMP_NUM_THREADS"] = "4"
+os.environ["KMP_BLOCKTIME"] = "30"
+os.environ["KMP_SETTINGS"] = "1"
+os.environ["KMP_AFFINITY"] = "granularity=fine,verbose,compact,1,0"
+
+K.set_session(session)
 
 
 @click.command()
